63ed0118f1ab998705898749829a38c6ccc6438f
tmojzes <tmojzi96@gmail.com>
2020-04-14 21:14:15 +0200

    Working if it gets enough frames but needs more memory to complete the whole video in one pano.

--- image_stiching.py
+++ image_stiching.py
@@ -1,9 +1,19 @@
 # import the necessary packages
 import numpy as np
-import imutils
+from imutils import paths
 import argparse
 import cv2
-from imutils import paths
+import os
+import math
+import re
+
+#Enable gpu accerelation
+# cv2.ocl.setUseOpenCL(True)
+
+#Round up numbers
+# def round_up(n, decimals=0):
+#     multiplier = 10 ** decimals
+#     return math.ceil(n * multiplier) / multiplier
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -13,9 +23,21 @@ ap.add_argument("-o", "--output", type=str, required=True,
 	help="path to the output image")
 args = vars(ap.parse_args())
 
+#Alphanumeric sorting implementation:
+def sorted_nicely(image_list):
+    """ Sorts the given iterable in the way that is expected. 
+    Required arguments: 
+    l -- The iterable to be sorted. 
+    """  
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]  
+    return sorted(image_list, key = alphanum_key)
+
 # grab the paths to the input images and initialize our images list
 print("[INFO] loading images...")
-imagePaths = sorted(list(paths.list_images(args["images"])))
+imagePaths = list(paths.list_images(args["images"]))
+imagePaths = sorted_nicely(imagePaths)
+print(imagePaths)
 images = []
 # loop over the image paths, load each one, and add them to our
 # images to stitch list
@@ -26,18 +48,42 @@ for imagePath in imagePaths:
 # initialize OpenCV's image stitcher object and then perform the image
 # stitching
 print("[INFO] stitching images...")
-stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
-(status, stitched) = stitcher.stitch(images)
+stitcher = cv2.Stitcher_create(mode=1)
+start =0
+diff = 333
+finish = start + diff
 
-# if the status is '0', then OpenCV successfully performed image
-# stitching
-if status == 0:
-	# write the output stitched image to disk
-    cv2.imwrite(args["output"], stitched)
-	# display the output stitched image to our screen
-    cv2.imshow("Stitched", stitched)
-    cv2.waitKey(0)
-    # otherwise the stitching failed, likely due to not enough keypoints)
-    # being detected
-else:
-    print("[INFO] image stitching failed ({})".format(status))
+# (status, stitched) = stitcher.stitch(images)
+# print(status)
+# cv2.imwrite(args["output"], stitched)
+
+for j in range(0, math.ceil(len(images)/diff)):
+    (status, stitched) = stitcher.stitch(images[:finish])
+    
+
+    # if the status is '0', then OpenCV successfully performed image
+    # stitching
+    if status == 0:
+        # write the output stitched image to disk
+        cv2.imwrite(f"{finish}"+args["output"], stitched)
+        # os.system(f"mv output.png output/output{finish}.png")
+        # display the output stitched image to our screen
+        # cv2.imshow("Stitched", stitched)
+        # otherwise the stitching failed, likely due to not enough keypoints)
+        # being detected
+        start+=math.ceil(diff*0.7)
+        finish+=diff
+    else:
+        print("[INFO] image stitching failed ({})".format(status))
+        break
+
+# last_start = (math.ceil(len(images)/diff)-1)*diff
+# print(last_start)
+# (status, stitched) = stitcher.stitch(images[last_start:])
+# if status == 0:
+#     cv2.imwrite(args["output"], stitched)
+#     os.system(f"mv output.png output/output{len(images)}.png")
+# else:
+#     print("[INFO] image stitching failed ({})".format(status))
+
+    # cv2.waitKey(0)
